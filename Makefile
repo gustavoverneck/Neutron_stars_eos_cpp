6310ffba62c6335ffb08fc79065f7fbf9b5587dd
Gustavo A. Verneck <gustavoverneck@gmail.com>
2024-07-08 15:28:47 -0300

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,3 @@
+main.x:
+	rm -f main.x
+	g++ -o main.x main.cpp -larmadillo -std=c++11
---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <stdio.h>
+#include <armadillo>
+
+// install armadillo from
+// https://www.uio.no/studier/emner/matnat/fys/FYS4411/v13/guides/installing-armadillo/
+
+// Functions
+double funcv1(double x) {
+    
+    
+    return x;
+}
+
+
+
+// Main
+
+int main() {
+
+    // Define variables
+    arma::vec fvec = arma::vec(4).fill(0.0);
+    arma::vec x = arma::vec(4);
+
+    // Vector of baryon masses
+    arma::vec mb = arma::vec(8);
+    mb(0) = 0.938272;
+    mb(1) = 1.115683;
+    mb(2) = 1.18937;
+    mb(3) = 1.192642;
+    mb(4) = 1.31486;
+    mb(5) = 1.32171;
+    mb(6) = 1.3872;
+    mb(7) = 1.540;
+    
+    double m = 0.938272; // Mass of nucleon / normalization constant
+    
+    // Vector of lepton masses
+    arma::vec ml = arma::vec(2);   // Vector of lepton masses
+    
+    // Meson masses
+    double ms; // mass of the scalar meson
+    double mv; // mass of the vector meson
+    double mr;
+
+
+
+    // Initial x ansatz
+    x(0) = 1.0; x(1) = 2.0; x(2) = 3.0; x(3) = 4.0;
+
+    for (int i = 0; i < 4; i++) {
+        std::cout << x(i) << std::endl;
+    }
+
+
+
+
+    // Initial x values
+
+
+
+
+
+    return 0;
+}
